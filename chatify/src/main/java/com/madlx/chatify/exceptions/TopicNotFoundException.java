52b9c2757c3d69e563bfb0dd9812c57748d9ba41
Administrator <mannukumar233@gmail.com>
2025-03-21 20:10:05 +0530

    added some exceptions

---
+++ chatify/src/main/java/com/madlx/chatify/exceptions/TopicNotFoundException.java
@@ -0,0 +1,15 @@
+package com.madlx.chatify.exceptions;
+
+public class TopicNotFoundException extends RuntimeException{
+    public TopicNotFoundException(String message) {
+        super(message);
+    }
+
+    public TopicNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TopicNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
