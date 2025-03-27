fb0af2f2732f6c1f7ce30551f00f64313409fa68
Administrator <mannukumar233@gmail.com>
2025-03-27 23:47:57 +0530

    updated the security configuration

--- chatify/src/main/java/com/madlx/chatify/config/SecurityConfig.java
+++ chatify/src/main/java/com/madlx/chatify/config/SecurityConfig.java
@@ -1,23 +1,23 @@
 package com.madlx.chatify.config;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.madlx.chatify.filters.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -26,10 +26,22 @@ import org.springframework.security.web.csrf.CsrfToken;
 public class SecurityConfig {
     @Autowired
     private  UserDetailsService userDetailsService;
+    @Autowired
+    private JwtFilter jwtFilter;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+       http.authorizeHttpRequests(request ->
+                        request.requestMatchers("/login", "/signup","/home")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+               .formLogin(AbstractHttpConfigurer::disable)
+               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return  http.build();
     }
 
